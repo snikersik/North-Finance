09daba030152079f1f4ed2f7ce06309ae1ee3d0c
xneeks <106173720+snikersik@users.noreply.github.com>
2026-07-13 01:48:21 +0200

    Add SwiftData transaction foundation

--- NorthFinance/App/NorthFinanceApp.swift
+++ NorthFinance/App/NorthFinanceApp.swift
@@ -1,11 +1,23 @@
+import SwiftData
 import SwiftUI
 
 @main
 struct NorthFinanceApp: App {
+    private let modelContainer: ModelContainer
+
+    init() {
+        do {
+            modelContainer = try ModelContainerFactory.makePersistentContainer()
+        } catch {
+            fatalError("Unable to create the local model container: \(error)")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             AppShellView()
         }
+        .modelContainer(modelContainer)
         .defaultSize(width: 1_100, height: 720)
     }
 }
