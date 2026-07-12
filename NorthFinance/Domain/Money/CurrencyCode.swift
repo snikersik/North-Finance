9f9f285f130a118a977075e37f6af6c8200ceb55
xneeks <106173720+snikersik@users.noreply.github.com>
2026-07-13 01:49:03 +0200

    Add SwiftData transaction foundation

---
+++ NorthFinance/Domain/Money/CurrencyCode.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum CurrencyCode: String, CaseIterable, Codable {
+    case pln = "PLN"
+    case eur = "EUR"
+    case usd = "USD"
+    case gbp = "GBP"
+    case chf = "CHF"
+}
