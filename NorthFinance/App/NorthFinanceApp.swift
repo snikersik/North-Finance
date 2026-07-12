38c8aabc37f5e7cee03d8cf4839e3bee50ec7157
xneeks <106173720+snikersik@users.noreply.github.com>
2026-07-12 23:51:42 +0200

    Create initial macOS application shell

---
+++ NorthFinance/App/NorthFinanceApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct NorthFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppShellView()
+        }
+        .defaultSize(width: 1_100, height: 720)
+    }
+}
